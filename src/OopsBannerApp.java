4842b2e904c8cbed50d880563c9a1053021ae107
va2277-eng <va2277@srmist.edu.in>
2026-03-03 14:09:32 +0530

    Update OopsBannerApp.java

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,10 +1,10 @@
 class OOPSBannerApp4 {
     static void main(String[] args) {
 
-        // Define array with 7 lines (7-line banner format)
+        
         String[] lines = new String[7];
 
-        // Populate banner lines for "OOPS"
+        
         lines[0] = String.join(" ", " ***** ", " ***** ", " ***** ", " ***** ");
         lines[1] = String.join(" ", "*     *", "*     *", "*     *", "*      ");
         lines[2] = String.join(" ", "*     *", "*     *", "*     *", "*      ");
@@ -13,7 +13,7 @@ class OOPSBannerApp4 {
         lines[5] = String.join(" ", "*     *", "*     *", "*      ", "*     *");
         lines[6] = String.join(" ", " ***** ", " ***** ", "*      ", " ***** ");
 
-        // Print using for-each loop
+        
         for (String line : lines) {
             System.out.println(line);
         }
